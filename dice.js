9ba910043b79e423d9607a538ca400e546119788
workforharmony <workforharmony@gmail.com>
2025-03-09 20:23:32 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 import { RoundedBoxGeometry } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/geometries/RoundedBoxGeometry.js";
 
 let scene, camera, renderer, dice, controls;
